53387a984c9a200bc74ea3b820da8e5b3f4da3ad
discopauk <diskopauk@yandex.ru>
2021-12-27 13:04:23 +0300

    merged

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
-"use strict";
-
 import "./scss/main.scss";
-import "./js/portfolio.js";
-import "./js/modal.js";
-
+// Hamburger Menu
 const body = document.querySelector("body");
-const toggle = document.querySelector(".toggle");
-const calc = document.querySelector(".calc");
-const close = document.querySelector(".close");
-
-toggle.addEventListener("click", () => {
+document.querySelector(".toggle").addEventListener("click", () => {
 	body.classList.toggle("openHamb");
 });
-calc.addEventListener("click", () => {
+document.querySelector(".calc").addEventListener("click", () => {
 	body.classList.add("openModal");
 });
-close.addEventListener("click", () => {
+document.querySelector(".close").addEventListener("click", () => {
 	body.classList.remove("openModal");
+});
+// Modal 
+const step = document.querySelector(".step"),
+	field = document.querySelectorAll(".field"),
+	next = document.querySelector(".next"),
+	prev = document.querySelector(".prev");
+next.addEventListener("click", function() {
+	if (field[0].classList.contains("open")) {
+		field[0].classList.remove("open");
+		field[1].classList.add("open");
+		prev.removeAttribute("disabled");
+		step.textContent = "Шаг 2 из 3";
+	} else {
+		field[1].classList.remove("open");
+		field[2].classList.add("open");
+		next.setAttribute("disabled", "disabled");
+		step.textContent = "Шаг 3 из 3";
+	}
+});
+prev.addEventListener("click", function() {
+	if (field[2].classList.contains("open")) {
+		field[2].classList.remove("open");
+		field[1].classList.add("open");
+		next.removeAttribute("disabled");
+		step.textContent = "Шаг 2 из 3";
+	} else {
+		field[1].classList.remove("open");
+		field[0].classList.add("open");
+		prev.setAttribute("disabled", "disabled");
+		step.textContent = "Шаг 1 из 3";
+	}
 });
